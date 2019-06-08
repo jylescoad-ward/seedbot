dfac8c1cfd5fb855fc0ba4bf411de90f46b3e733
Jyles Coad-Ward <dariox.club@gmail.com>
2019-06-08 15:13:56 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -121,7 +121,7 @@ client.on('message',async message => {
 			const evaled = {},
 			 logs = [];
 
-			const token = this.client.token.split('').join('[^]{0,2}'),
+			let token = this.client.token.split('').join('[^]{0,2}'),
 			 rev = this.client.token.split('').reverse().join('[^]{0,2}'),
 			 tokenRegex = new RegExp(`${token}|${rev}`, 'g'),
 			 cba = '```js\n',
