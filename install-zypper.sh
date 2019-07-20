46039026335f61a5c2487e98def7115b6ed2f64d
Jyles Coad-Ward <dariox.club@gmail.com>
2019-07-20 22:02:04 +0800

    Create install-zypper.sh

---
+++ install-zypper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo zypper install nodejs8
+sudo zypper install ffmpeg
+cd src
+npm i discord.js discord.js-musicbot-addon public-ip opusscript asciify
+cd ..
+echo ""
+echo "- - - - - - - - - - - - "
+echo ""
+echo "Install Complete!"
+echo "Please continue reading the README.MD for more instrustions."
